384d922fa9498c69875f58601fb0b8dc79782f50
h493 <chhikarahimanshu21@gmail.com>
2025-04-19 19:18:22 +0530

    signin and signup

--- src/main/java/com/ecommerce/project/repositories/UserRepository.java
+++ src/main/java/com/ecommerce/project/repositories/UserRepository.java
@@ -1,6 +1,9 @@
 package com.ecommerce.project.repositories;
 
 import com.ecommerce.project.model.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +11,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserName(String userName);
     Boolean existsByUserName(String userName);
+
+    Boolean existsByEmail(String email);
 }
