8adc1ce4e9423a37afd97fa16642f276712880a7
h493 <chhikarahimanshu21@gmail.com>
2025-04-26 07:48:47 +0530

    Repository annotation added

--- src/main/java/com/ecommerce/project/repositories/UserRepository.java
+++ src/main/java/com/ecommerce/project/repositories/UserRepository.java
@@ -5,9 +5,10 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserName(String userName);
     Boolean existsByUserName(String userName);
